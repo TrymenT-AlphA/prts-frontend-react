f4f97a3b648e7efd9b64ac65b3711b153f5da57c
AliceRemake <1302393176@qq.com>
2023-02-28 01:07:38 +0800

    home-page

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM node:16
+FROM node
 WORKDIR /app
 
 COPY package.json .
 COPY yarn.lock .
 
 # Setup yarn
+RUN npm set registry https://mirrors.huaweicloud.com/repository/npm/
 RUN yarn install
 
 COPY . .
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -7,17 +7,38 @@
  * @author Alice Remake
  *
  * Created at     : 2022-12-27 00:32:29
- * Last modified  : 2022-12-29 13:44:00
+ * Last modified  : 2023-02-28 01:06:52
  */
 
 import Toolbar from "../components/toolbar";
+import { useEffect } from "react";
+import DameDaneParticle from "../utils/damedane";
 
 function HomePage() {
+  useEffect(() => {
+    var canvas = document.getElementById("akCanvas");
+    var damedane = new DameDaneParticle(canvas, {
+      src: "./logo/island.png",
+      renderX: 100,
+      renderY: 100,
+      w: 300,
+      size: 1,
+      spacing: 2,
+      validColor: {
+        min: 300,
+        max: 765,
+        invert: false,
+      },
+      effectParticleMode: "repulsion",
+      Thickness: 25,
+    });
+  }, []);
+
   return (
     <>
       <Toolbar currentPage={`首页`} />
-      <div className="mdui-container mdui-p-t-4 mdui-p-b-5">
-        <div className="mdui-typo-display-4 mdui-text-center">PRTS 终端</div>
+      <div id="akCanvasContainer">
+        <canvas id="akCanvas" />
       </div>
     </>
   );
